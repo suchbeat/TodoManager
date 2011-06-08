cb198591ca0c6053613fe6fae6882e58bc86d08a
Yuriy <yuriy@yuriy-desktop.(none)>
2011-06-08 11:37:02 +0300

    added check for login

--- views.py
+++ views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 import datetime
 
 def homepage(request):
-	return HttpResponse("Home page")
+	if request.user.is_authenticated():
+		return HttpResponse("Home page")
+	else:
+		return render_to_response('login.html')
 
 def hello(request):
 	return HttpResponse("Hello world")
