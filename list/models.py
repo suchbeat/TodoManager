a6d4b38a0b31afd7428cc708b52503e15a07dce0
yuriy <yuriy@yuriy-desktop.(none)>
2011-06-13 00:59:47 +0300

    added list app

---
+++ list/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Item(models.Model):
+	title = models.CharField(max_length=50)
+	description = models.TextField()
+	user = models.ForeignKey(User)
+	creator_id = models.IntegerField()
+	creation_date = models.DateTimeField()
+
+	def __unicode__(self):
+		return self.title
